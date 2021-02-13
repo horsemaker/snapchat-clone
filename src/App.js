e7aaad95a80bd35451eadc707de4ead4dc61a846
horsemaker <yash_ghodekar@yahoo.in>
2021-02-13 17:34:22 +0530

    Production Ready.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react'; // 2.33.03
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import Preview from './Preview';
 import WebcamCapture from './WebcamCapture';
+import Chats from './Chats';
+import ChatView from './ChatView';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/appSlice';
+import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(login({
+          username: authUser.displayName,
+          profilePic: authUser.photoURL,
+          id: authUser.uid,
+        }))
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <WebcamCapture />
+    <div className="app">
+      <Router>
+        {
+          !user ? (
+            <Login />
+          ) : (
+            <>
+              <img className="app__logo" src="https://lakeridgenewsonline.com/wp-content/uploads/2020/04/snapchat.jpg" alt=""/>
+              <div className="app__body">
+                <div className="app__bodyBackground">
+                  <Switch>
+                    <Route path="/chats/view">
+                      <ChatView />
+                    </Route>
+                    <Route path="/chats">
+                      <Chats />
+                    </Route>
+                    <Route path="/preview">
+                      <Preview />
+                    </Route>
+                    <Route exact path="/">
+                      <WebcamCapture />
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+            </>
+          )
+        }
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+// deployment
+// firebase init
+// hosting -> spacebar -> enter
+// use an existing project
+// select project
+// build
+// y
+// N
+// npm run build
+// firebase deploy
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,19 +1,27 @@
 import React, { useRef, useCallback } from 'react';
 import Webcam from 'react-webcam';
+import './WebcamCapture.css';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { useDispatch } from 'react-redux';
+import { setCameraImage } from './features/cameraSlice';
+import { useHistory } from 'react-router-dom';
 
 const videoConstraints = {
-    width: 250,
-    height: 400,
+    width: 350,
+    height: 550,
     facingMode: "user",
 };
 
 function WebcamCapture() {
 
     const webcamRef = useRef(null);
+    const dispatch = useDispatch();
+    const history = useHistory();
 
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
+        dispatch(setCameraImage(imageSrc));
+        history.push('/preview')
     }, [webcamRef])
 
     return (
@@ -30,7 +38,6 @@ function WebcamCapture() {
             <RadioButtonUncheckedIcon 
                 className='webcamCapture__button'
                 onClick={capture}
-                fontSize="large"
             />
         </div>
     )
